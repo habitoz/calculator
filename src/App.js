1dd05878f12ab474e6a3846cf7a23d6c63286660
haben mehar <habenmehari71@gmail.com>
2023-07-14 14:55:35 +0300

    loading and git status

--- src/App.js
+++ src/App.js
@@ -5,22 +5,10 @@ import Header from './components/Header';
 import Qoute from './components/Qoute';
 import Home from './components/Home';
 
-const fetchQuote = async (cb) => {
-  const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=happiness', {
-    headers: {
-      'X-Api-Key': 'uHRdrDXaqWLwqBrwbx6P9A==Bk4Cvlvw6ePt3sW1',
-    },
-  });
-  const data = await response.json();
-  cb(data);
-};
-
 function App() {
-  const [quotes, setQuotes] = useState([]);
   const [tab, setTab] = useState();
   useEffect(() => {
     setTab('Home');
-    fetchQuote(setQuotes);
   }, []);
 
   return (
@@ -44,7 +32,7 @@ function App() {
         {
         tab === 'Quote' && (
           <section className="QuoteContainer">
-            <Qoute quotes={quotes} />
+            <Qoute />
           </section>
         )
       }
